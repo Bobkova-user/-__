2f84fa621d68d1e20478140572e4a03b84bf7edb
Bobkova-user <bobr30090@gmail.com>
2024-10-17 13:14:08 +0300

    дз по технологии 3.py

--- дз по технологии 3.py
+++ дз по технологии 3.py
@@ -1,5 +1,4 @@
 hour = int(input("Введите время в часах (0-23): "))
-
 if (hour < 8) or (hour >= 22) or (10 <= hour < 12) or (14 <= hour < 15) or (18 <= hour < 20):
     print("Посылку получить нельзя.")
 else:
